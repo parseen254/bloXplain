86b1417624dd14dcfe80a26b55ee40b60d8607cb
David Parseen <hello@parseen.dev>
2023-01-21 01:04:45 +0300

    Feature: Adds tailwind and cleans up UI

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -2,9 +2,8 @@ import { Button } from "ui";
 
 export default function Web() {
   return (
-    <div>
-      <h1>Web</h1>
-      <Button />
+    <div className="w-full min-h-screen flex justify-center items-center">
+      <h1 className="text-3xl font-bold underline">bloXplain</h1>
     </div>
   );
 }
