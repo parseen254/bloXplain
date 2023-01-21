b264213d48ab344effdebead995b36a84c5c1d30
David Parseen <hello@parseen.dev>
2023-01-22 00:14:35 +0300

    Feature: Add UI for explanations and comments sections

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,9 +1,11 @@
 import { MagnifyingGlassIcon, QueueListIcon } from "@heroicons/react/20/solid";
 
+import { RightSideSection } from "ui";
+
 export default function Web() {
   return (
-    <div className="bg-white">
-      <div className="mx-auto max-w-7xl py-16 px-6 sm:py-24 lg:px-8">
+    <div className="bg-gray-200">
+      <div className="mx-auto max-w-7xl py-16 px-6 sm:py-24 lg:px-8 bg-indigo-100">
         <div className="text-center">
           <p className="mt-1 text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl lg:text-6xl">
             Welcome to bloXplain
@@ -26,23 +28,27 @@ export default function Web() {
                   type="text"
                   name="transaction_id"
                   id="tx_id"
-                  className="block w-full rounded-none rounded-l-md border-gray-300 pl-10 focus:border-fuchsia-500 focus:ring-fuchsia-500 sm:text-sm"
+                  className="block w-full rounded-none rounded-l-md border-gray-300 pl-10 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                   placeholder="Enter Transaction ID"
                 />
               </div>
               <button
                 type="button"
-                className="relative -ml-px inline-flex items-center space-x-2 rounded-r-md border border-gray-300 bg-gray-50 px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100 focus:border-fuchsia-500 focus:outline-none focus:ring-1 focus:ring-fuchsia-500"
+                className="relative -ml-px inline-flex items-center space-x-2 rounded-r-md border border-gray-300 bg-gray-50 px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-100 focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500"
               >
                 <MagnifyingGlassIcon
                   className="h-5 w-5 text-gray-400"
                   aria-hidden="true"
                 />
-                <span>Explain</span>
+                <span>Fetch Transaction</span>
               </button>
             </div>
           </div>
         </div>
+        <section className="flex">
+          <div className="flex-1"></div>
+          <RightSideSection />
+        </section>
       </div>
     </div>
   );
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,2 +1,3 @@
 import * as React from "react";
 export * from "./components/NavBar";
+export * from "./components/RightSideSection";
