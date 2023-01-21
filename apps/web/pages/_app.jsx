8dce1250609aed95b8db6146a23dcc5f4b4addb2
David Parseen <hello@parseen.dev>
2023-01-21 04:26:12 +0300

    Chore: Fix Minor UI issues

--- apps/web/pages/_app.jsx
+++ apps/web/pages/_app.jsx
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <NavBar />
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </>
   );
 }
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,7 +1,5 @@
-
 export default function Web() {
   return (
-    <div className="w-full min-h-full flex flex-col gap-4 justify-center items-center">
-    </div>
+    <div className="w-full min-h-full flex flex-col gap-4 justify-center items-center"></div>
   );
 }
