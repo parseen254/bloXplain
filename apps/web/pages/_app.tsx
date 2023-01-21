6be4cfe3874d23262d8a21918e4086496fd6f78d
David Parseen <hello@parseen.dev>
2023-01-21 11:28:43 +0300

    Chore: Make _app a typescript file

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,8 +1,9 @@
 import "../styles/global.css";
 
+import type { AppProps } from "next/app";
 import { NavBar } from "ui";
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <NavBar />
