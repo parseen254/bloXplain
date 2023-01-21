9137f91f27c3857ac27a3af3c55af55bb560d1d8
David Parseen <hello@parseen.dev>
2023-01-21 11:44:12 +0300

    Feature: Adds Rainbowkit for connect button

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,13 +1,39 @@
 import "../styles/global.css";
+import "@rainbow-me/rainbowkit/styles.css";
+
+import { RainbowKitProvider, getDefaultWallets } from "@rainbow-me/rainbowkit";
+import { WagmiConfig, configureChains, createClient } from "wagmi";
+import { arbitrum, mainnet, optimism, polygon } from "wagmi/chains";
 
 import type { AppProps } from "next/app";
 import { NavBar } from "ui";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider } = configureChains(
+  [mainnet, polygon, optimism, arbitrum],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "bloXplain",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <NavBar />
-      <Component {...pageProps} />
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains}>
+          <NavBar />
+          <Component {...pageProps} />
+        </RainbowKitProvider>
+      </WagmiConfig>
     </>
   );
 }
