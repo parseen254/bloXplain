869a56a45d172df96a0f860de0121070d4468bb4
David Parseen <hello@parseen.dev>
2023-01-21 14:40:37 +0300

    Feature: Adds hyperspace testnet chain

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -3,14 +3,38 @@ import "@rainbow-me/rainbowkit/styles.css";
 
 import { RainbowKitProvider, getDefaultWallets } from "@rainbow-me/rainbowkit";
 import { WagmiConfig, configureChains, createClient } from "wagmi";
-import { arbitrum, mainnet, optimism, polygon } from "wagmi/chains";
 
 import type { AppProps } from "next/app";
+import { Chain } from "wagmi/chains";
 import { NavBar } from "ui";
 import { publicProvider } from "wagmi/providers/public";
 
+const hyperSpaceChain: Chain & { iconUrl: string; iconBackground: string } = {
+  id: 3141,
+  name: "Filecoin – Hyperspace testnet",
+  network: "hyperspace",
+  iconUrl: "https://avatars.githubusercontent.com/u/22014611?s=200&v=4",
+  iconBackground: "#fff",
+  nativeCurrency: {
+    decimals: 18,
+    name: "FILECOIN",
+    symbol: "fTIL",
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://api.hyperspace.node.glif.io/rpc/v0"],
+    },
+    public: {
+      http: ["https://api.hyperspace.node.glif.io/rpc/v0"],
+    },
+  },
+  blockExplorers: {
+    default: { name: "Filfox", url: "https://hyperspace.filfox.info/en" },
+  },
+  testnet: true,
+};
 const { chains, provider } = configureChains(
-  [mainnet, polygon, optimism, arbitrum],
+  [hyperSpaceChain],
   [publicProvider()]
 );
 
